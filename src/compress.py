75d5d27c3bb24df5ba58990dec824bf917967fe8
MarcoAntonioGritti <m9946658@gmail.com>
2025-03-25 10:16:29 -0300

    Segundo commit: Modificando funções para melhor desempenho.

--- src/compress.py
+++ src/compress.py
@@ -1,5 +1,5 @@
 import os 
-import zipfile
+import zipfile #Não possui funções assíncronas
 
 PASTA_PDFS = "../data"
 PASTA_SAIDA = "../output"
@@ -9,10 +9,13 @@ NOME_ARQUIVO_ZIP = os.path.join(PASTA_SAIDA, "documentos.zip")
 # Criar diretório caso não exista
 os.makedirs(PASTA_SAIDA, exist_ok=True)
 
+#Faz a compactação dos PDFs baixados
 def compactar_pdfs():
     with zipfile.ZipFile(NOME_ARQUIVO_ZIP,"w",zipfile.ZIP_DEFLATED) as zipf:
         for arquivo in os.listdir(PASTA_PDFS):
             caminho_completo = os.path.join(PASTA_PDFS, arquivo)
+            print("arquivo", arquivo)
+            print("caminho_completo", caminho_completo)
             zipf.write(caminho_completo, arquivo)
 
     print(f"✅ Arquivo ZIP criado com sucesso: {NOME_ARQUIVO_ZIP}")
